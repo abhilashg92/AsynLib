a84e647950c13bd1a124f4b7998d96af58e2eb77
abhilashg92 <ghogaleabhilash92@gmail.com>
2025-03-31 13:17:56 +0530

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AsynLib",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "AsynLib",
+            targets: ["AsynLib"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "AsynLib"),
+        .testTarget(
+            name: "AsynLibTests",
+            dependencies: ["AsynLib"]
+        ),
+    ]
+)
