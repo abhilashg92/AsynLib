05017f24751d53de8c3e3259efb64069b748a88c
abhilashg92 <ghogaleabhilash92@gmail.com>
2025-04-01 15:12:10 +0530

    Initial commit

--- Sources/AsynLib/AsynLib.swift
+++ Sources/AsynLib/AsynLib.swift
@@ -1,2 +1,11 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import UIKit
+
+public struct MyPakage
+{
+    func sayHello() -> String{
+       return "Hello world!"
+    }
+}
